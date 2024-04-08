0504223f79f073840b416a052d0ca9ece08139ab
IgnacioGarcia2009 <166016602+IgnacioGarcia2009@users.noreply.github.com>
2024-04-08 07:59:25 -0300

    trabajo practico N°1

---
+++ java.js
@@ -0,0 +1,19 @@
+let numero1 = 100
+let numero2 = 50
+let suma = numero1 + numero2; 
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2; 
+console.log
+
+
+alert ("la suma es " + suma)
+alert ("la resta es " + resta)
+alert ("la multiplicacion es " + multiplicacion)
+alert ("la division es " + division)
+
+
+let cuandonacio = prompt("cuando naciste")
+let añoactual = 2024
+let resultadoedad = añoactual - cuandonacio
+alert ("la edad es: " + resultadoedad)
